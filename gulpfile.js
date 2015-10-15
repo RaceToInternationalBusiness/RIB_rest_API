9be2a5d05db5085a0dfdc9d82acfaa7d616a6820
Dralagen <dralagen@dralagen.fr>
2015-10-15 15:25:45 +0200

    use gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,47 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    gulpif = require('gulp-if'),
-    //concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    plumber = require('gulp-plumber'),
-    sourcemaps = require('gulp-sourcemaps'),
+    plugins = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'gulp.*'], // the glob(s) to search for
+        scope: ['dependencies', 'devDependencies', 'peerDependencies'], // which keys in the config to look within
+        replaceString: /^gulp(-|\.)/, // what to remove from the name of the module when adding it to the context
+        camelize: true,
+        lazy: true,
+        rename: {
+            'gulp-if':'gulpif'
+        }
+    }),
 
-    argv = require('yargs').argv,
+    argv = require('yargs').argv;
 
-    clean = require('gulp-rimraf'),
-
-    paths = {
-        scripts_src: ['./src/**/*.js'],
-        scripts_build: './build/js'
-    };
+var paths = {
+    scripts_src: ['./src/**/*.js'],
+    scripts_build: './build/js'
+};
 
 // Not all tasks need to use streams
 // A gulpfile is just another node program and you can use all packages available on npm
 gulp.task('clean', function(cb) {
     // You can use multiple globbing patterns as you would with `gulp.src`
     return gulp.src('build', {read: false})
-        .pipe(clean());
+               .pipe(plugins.rimraf());
 });
 
 gulp.task('scripts', ['clean'], function() {
     // Minify and copy all JavaScript (except vendor scripts)
     // with sourcemaps all the way down
     return gulp.src(paths.scripts_src)
-        .pipe(plumber())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'))
-        .pipe(gulpif(argv.prod, sourcemaps.init()))
-        .pipe(gulpif(argv.prod, uglify()))
-        //.pipe(concat('app.js'))
-        .pipe(gulpif(argv.prod, sourcemaps.write()))
-        .pipe(gulp.dest(paths.scripts_build))
+               .pipe(plugins.gulpif(!argv.prod, plugins.plumber()))
+               .pipe(plugins.jshint())
+               .pipe(plugins.jshint.reporter('jshint-stylish'))
+               .pipe(plugins.jshint.reporter('fail'))
+               .pipe(plugins.gulpif(argv.prod, plugins.sourcemaps.init()))
+               .pipe(plugins.gulpif(argv.prod, plugins.uglify()))
+               //.pipe(concat('app.js'))
+               .pipe(plugins.gulpif(argv.prod, plugins.sourcemaps.write()))
+               .pipe(gulp.dest(paths.scripts_build))
 });
 
 gulp.task('run', ['scripts'], function() {
-
-    var server = require('gulp-express');
-    server.run(['bin/www']);
-
+    plugins.express.run(['bin/www']);
 });
 
 // Rerun the task when a file changes
