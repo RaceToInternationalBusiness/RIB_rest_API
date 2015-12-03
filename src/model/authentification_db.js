8119e0b13abbb74d2a3f986d0b4d4db94da4325f
perraid <davidperrai@gmail.com>
2015-12-03 17:32:41 +0100

    adding missing authentification files

---
+++ src/model/authentification_db.js
@@ -0,0 +1,21 @@
+/**
+ * New node file
+ */
+var mongoose = require('mongoose');
+
+var mongoStat = mongoose.connection.readyState;
+
+if (mongoStat !== 1 && mongoStat !== 2) {
+    mongoose.connect(process.env.MONGOLAB_URI);
+}
+
+// create instance of Schema
+// var mongoSchema = mongoose.Schema;
+// create schema
+var authentification = {
+    'login': String,
+    'password': String
+};
+// create model if not exists.
+
+module.exports = mongoose.model('authentification', authentification);
