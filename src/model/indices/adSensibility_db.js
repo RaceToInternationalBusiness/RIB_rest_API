b1407e821817d3918e762fe468c80e8a42cdab98
perraid <davidperrai@gmail.com>
2015-12-10 17:23:07 +0100

    adding missing files

---
+++ src/model/indices/adSensibility_db.js
@@ -0,0 +1,24 @@
+/**
+ * New node file
+ */
+var mongoose = require('mongoose');
+
+var mongoStat = mongoose.connection.readyState;
+
+if (mongoStat !== 1 && mongoStat !== 2) {
+    mongoose.connect(process.env.MONGOLAB_URI);
+}
+
+// create instance of Schema
+// var mongoSchema = mongoose.Schema;
+// create schema
+var adsensibility = {
+    'price': String,
+    'products': {
+        'name': String,
+        'index': Number
+    }
+};
+// create model if not exists.
+
+module.exports = mongoose.model('adsensibility', adsensibility);
