83694c5f040c1a6628f4d10280d293321d1ae9c4
perraid <davidperrai@gmail.com>
2015-12-30 17:24:04 +0100

    changing product test

--- test/productTest.js
+++ test/productTest.js
@@ -5,10 +5,8 @@ var server = supertest.agent("http://localhost:3000");
 
 var myProduct = {
     "name": "Champagne",
-    "price": 180,
-    "coefAd": 1.5,
-    "coefMerch": 1.5,
-    "delay": 6
+    "priceIndex": [],
+    "advertising": []
 };
 var myProductId;
 var myMarketId;
@@ -16,43 +14,39 @@ var myMarketId;
 describe('Product rest', function() {
 
     it('should create new product on my market', function(done) {
-        server.post('/market')
-            .send({name: 'Champagne'})
-            .end(function(err, res) {
-                myMarketId = res.body._id;
-                server.post('/market/' + myMarketId + '/product')
-                    .send(myProduct)
-                    .expect('Content-type', /json/)
-                    .expect(200, done)
-                    .expect(function(res) {
+        server.post('/market').send({
+            name: 'Champagne'
+        }).end(
+                function(err, res) {
+                    myMarketId = res.body._id;
+                    server.post('/market/' + myMarketId + '/product').send(
+                            myProduct).expect('Content-type', /json/).expect(
+                            200, done).expect(function(res) {
                         res.body.should.have.property('_id');
                         myProductId = res.body._id;
                         myProduct['_id'] = myProductId;
                         myProduct['__v'] = 0;
                         myProduct['market'] = myMarketId;
                     }, done);
-            });
+                });
     });
 
     it('should get one product', function(done) {
-        server.get('/market/' + myMarketId + '/product/' + myProductId)
-            .expect('Content-type', /json/)
-            .expect(200, done)
+        server.get('/market/' + myMarketId + '/product/' + myProductId).expect(
+                'Content-type', /json/).expect(200, done)
 
-            .expect(function(res) {
-                res.body.should.eql(myProduct);
-            }, done);
+        .expect(function(res) {
+            res.body.should.eql(myProduct);
+        }, done);
     });
 
     it('should get all product', function(done) {
-        server.get('/market/' + myMarketId + '/products')
-            .expect('Content-type', /json/)
-            .expect(200, done)
+        server.get('/market/' + myMarketId + '/products').expect(
+                'Content-type', /json/).expect(200, done)
 
-            .expect(function(res) {
-                res.body.should.be.an.Array;
-                res.body.should.eql([myProduct]);
-            }, done);
+        .expect(function(res) {
+            res.body.should.be.an.Array;
+            res.body.should.eql([myProduct]);
+        }, done);
     });
 });
-
