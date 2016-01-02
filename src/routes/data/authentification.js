1e348aaecbe8afd6f28ff33bac25e5b00095a3dd
perraid <davidperrai@gmail.com>
2016-01-02 14:10:01 +0100

    removing all unless 404 error from routes close #6

--- src/routes/data/authentification.js
+++ src/routes/data/authentification.js
@@ -140,10 +140,4 @@ router.post('/', function(req, res) {
     }
 });
 
-router.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
 module.exports = router;
--- src/routes/data/results.js
+++ src/routes/data/results.js
@@ -155,10 +155,4 @@ router.post('/result', function(req, res) {
     });
 });
 
-router.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
 module.exports = router;
